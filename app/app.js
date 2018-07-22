feda952cb35b022d95ab44cf4f1a17c5e2bef8c4
nonsosky <nonsoskyokpara@gmail.com>
2018-07-22 16:45:19 +0100

    first commit

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ app.set('views', 'app/views');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static('app/public'));
-app.use(require('./routers/index'));
+app.use(require('./routes/index'));
 
 app.listen(app.get('port'), () => {
   console.log('Server is listening on port ' + app.get('port'));
